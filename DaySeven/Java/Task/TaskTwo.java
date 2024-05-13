3cab3e7e6a181d1441759b9ba3169bd2c22afe50
janetoluwafemi <Oluwafemijanet85@gmail.com>
2024-05-13 17:01:37 +0100

    work

--- DaySeven/Java/Task/TaskTwo.java
+++ DaySeven/Java/Task/TaskTwo.java
@@ -2,74 +2,77 @@ import java.util.Scanner;
 	public class TaskTwo{
 		public static void main(String[] args){
 			Scanner scanner = new Scanner(System.in);
+			double depositAmount = 0.0;
+			double withdrawAmount = 0.0;
+			double transferAmount = 0.0;
 
 			System.out.print("Enter first name: ");
-			String firstName = scanner.nextLine();
+			String firstName = scanner.next();
 
 			System.out.print("Enter last name: ");
-			String lastName = scanner.nextLine();
+			String lastName = scanner.next();
 
 			System.out.print("Enter your account number: ");
-			int accountNumber = scanner.nextInt();
-		
-			System.out.print("Enter deposit amount: ");
-			double depositAmount = scanner.nextDouble();
+			String accountNumber = scanner.next();
+			System.out.print("Which one do you want: ");
+			String answer = scanner.next();
+			if(answer == "deposit"){
+				System.out.print("Enter deposit amount: ");
+				depositAmount = scanner.nextDouble();
+			}
+			if(answer == "withdraw"){
+				System.out.print("Enter the amount you want to redraw: ");
+				withdrawAmount = scanner.nextDouble();
 
-			System.out.print("Enter the amount you want to redraw: ");
-			double amountWithdraw = scanner.nextDouble();
-
-			System.out.print("Do you want to make a transfer: ");
-			String transfer = scanner.nextLine();
-
-	
-			System.out.print("How much do you want to transfer: ");
-			double amount = scanner.nextDouble();
+			}
+			if(answer == "transfer"){ 
+				System.out.print("How much do you want to transfer: ");
+				transferAmount = scanner.nextDouble();
+			}
 
 			System.out.print("Which bank are you transfering to: ");
-			String bankName = scanner.nextLine();
+			String bankName = scanner.next();
 
 			System.out.print("Which account are you transfering to: ");
-			int personAccountNumber = scanner.nextInt();
+			String personAccountNumber = scanner.next();
 
-			double checkBalance = depositAmount - amountWithdraw;
+			double checkBalance = depositAmount - withdrawAmount;
+			
 			
 			
 			int pin;
-			pin = 1234;
+	
 			System.out.print("Enter your pin:");
 			int accountPin = scanner.nextInt();
 
-			if(pin == accountPin){
-				System.out.print("Transfer successful");
-			}
-			else{
-				System.out.println("Wrong pin");
-			}
 			
 			System.out.print("Do you want to change pin:");
-			String changePin = scanner.nextLine();
-			
+			String changePin = scanner.next();
+			 
 			if(changePin == "yes"){
 				System.out.print("Enter new pin:");
 				int newPin = scanner.nextInt();
 				pin = newPin;
 			}
 			
-			double result = accountAmount(depositAmount, amountWithdraw);
+			double result = accountAmount(depositAmount, withdrawAmount);
 			System.out.print(result);
 
 			double result2 = balance(checkBalance, amount);
 			System.out.print(result2);
 		}
 		
-		public static double accountAmount(double depositAmount, double amountWithdraw){
-			double checkBalance = depositAmount - amountWithdraw;
+		public static double accountAmount(double depositAmount, double withdrawAmount){
+			double checkBalance = depositAmount - withdrawAmount;
+
 			return checkBalance;
 		}
 
 		public static double balance(double checkBalance, double amount){
 			double balance = checkBalance - amount;
-
+	
 			return balance;
+
+			
 		}
 	}		
