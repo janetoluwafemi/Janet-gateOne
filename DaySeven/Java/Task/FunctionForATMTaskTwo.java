be7fcadc9c52c12aa53a961d8a7cf874b0546184
janetoluwafemi <Oluwafemijanet85@gmail.com>
2024-05-13 23:58:15 +0100

    work

--- DaySeven/Java/Task/FunctionForATMTaskTwo.java
+++ DaySeven/Java/Task/FunctionForATMTaskTwo.java
@@ -12,28 +12,62 @@ import java.util.ArrayList;
 		int pin = 0;
 		
 		public void creatingAccount(){
-			System.out.println("Welcome To Semicolon Bank");
-			System.out.print("Do you want to create an account:");
-			String answer = scanner.next();
-			if(reply .equals("yes")){
-				System.out.print("Enter first name: ");
-				firstName.add(scanner.next());
+			System.out.print("Enter first name: ");
+			firstName.add(scanner.next());
 
-				System.out.print("Enter last name: ");
-				lastName.add(scanner.next());
+			System.out.print("Enter last name: ");
+			lastName.add(scanner.next());
 	
-				 System.out.print("Enter your account number: ");
-				accountNumber.add(scanner.next());
+			System.out.print("Enter your account number: ");
+			accountNumber.add(scanner.next());
 
-				System.out.print("Enter your pin: ");
-				pin.add(scanner.nextInteger());
-	
-				System.out.print("Enter your account balance: ");
-				balance.add(scanner.nextDouble());
+			System.out.print("Enter your pin: ");
+			pin.add(scanner.nextInteger());
+
+			balance.add(0.0);
 
 		}
-		public void 
+		public void deposit(){
+			System.out.print("Enter the account number: ");
+			String userAccountNumber = scanner.nextInt();
+			System.out.print("How much do you want to deposit: ");
+			double depositAmount = scanner.nextDouble();
+			for(int counter = 0; counter < accountNumber; counter++){
+				if(accountNumber .equals(accountNumber.get(counter)){ 	
+					double balance.set(counter,depositAmount + balance.get(counter));
+				}
+			}
+			
+		}
+		public void withdraw(){
+			System.out.print("Enter the account number: ");
+			String accountNumber = scanner.nextInt();	
+
+			System.out.print("How much do you want to withdraw: ");
+			double amountWithdrawn = scanner.nextDouble;
+			for(int counter = 0; counter < accountNumber; counter++){
+				if(accountNumber .equals(accountNumber.get(counter)){ 
+					double balance.set(counter,amountBalance.get(counter) - amountWithdrawn);
+				}
+			}
+		}
+		public void balance(){
+			System.out.print("Enter your account number: ");
+			String accountNo = scanner.next();
+			for(int counter = 0; counter < accountNumber; counter++){
+				if(accountNo .equals(accountNumber.get(counter)){ 
+					System.out.print(firstName.get(counter));
+					System.out.print(lastName.get(counter));
+					System.out.print(accaountNo.get(counter));
+					System.out.print(balance.get(counter));
+				}
+			}
+		}
+	
+		
+			
+
 		
 		
 			
-}
+	 }
