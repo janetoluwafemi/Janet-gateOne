3057b7529102c9b3042207a0dc5710ccc7d27d82
janetoluwafemi <Oluwafemijanet85@gmail.com>
2024-05-13 21:21:41 +0100

    work

---
+++ DaySeven/Java/Task/FunctionForATMTaskTwo.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+	public class FunctionForATMTaskTwo{
+		private Scanner scanner = new Scanner(System.in);
+		ArrayList<String> firstName = new <String>ArrayList();
+		ArrayList<String> lastName = new <String>ArrayList();
+		ArrayList<Integer> pin = new <Integer>ArrayList();
+		ArrayList<String> accountNumber = new <String>ArrayList();
+		ArrayList<Double> balance = new <Double>ArrayList();
+ 		String accountNumber = "";
+		double balance = 0.0;
+		int pin = 0;
+		
+		public void creatingAccount(){
+			System.out.println("Welcome To Semicolon Bank");
+			System.out.print("Do you want to create an account:");
+			String answer = scanner.next();
+			if(reply .equals("yes")){
+				System.out.print("Enter first name: ");
+				firstName.add(scanner.next());
+
+				System.out.print("Enter last name: ");
+				lastName.add(scanner.next());
+	
+				 System.out.print("Enter your account number: ");
+				accountNumber.add(scanner.next());
+
+				System.out.print("Enter your pin: ");
+				pin.add(scanner.nextInteger());
+	
+				System.out.print("Enter your account balance: ");
+				balance.add(scanner.nextDouble());
+
+		}
+		public void 
+		
+		
+			
+}
